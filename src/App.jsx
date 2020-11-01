a53efca86858b0da18956c1c2f4d2ad2fdd4bf90
Alexey Delendik <delendikalexey@gmail.com>
2020-11-01 17:33:50 +0300

    create static background, logo, menu and turbina

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import Logo from './components/Logo';
+import Menu from './components/Menu';
+import Turbina from './components/Turbina';
+
 
 function App() {
   return (
     <div className = "page">
       <div className = "page-container">
-
+        <Logo />
+        <Menu />
+        <Turbina />
       </div>
     </div>
   );
