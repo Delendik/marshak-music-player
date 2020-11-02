24c0800ea7c76fc77a63446878bd74225c1b3240
Alexey Delendik <delendikalexey@gmail.com>
2020-11-02 20:41:28 +0300

    change margin in logo

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 function Menu () {
   return(
     <nav className = "navigation">
-      <a href="https://vk.com/vkmusic" target="_blank" title = "VK Music" className="navigation__link">Яндекс.Музыка ↗</a>
-      <a href="https://vk.com/vkmusic" target="_blank" title = "VK Music" className="navigation__link">Spotify ↗</a>
-      <a href="https://vk.com/vkmusic" target="_blank" title = "VK Music" className="navigation__link">Apple Music ↗</a>
-      <a href="https://vk.com/vkmusic" target="_blank" title = "VK Music" className="navigation__link">VK Music ↗</a>
+      <a href="https://vk.com/vkmusic" target="_blank" rel="noreferrer" title = "VK Music" className="navigation__link">Яндекс.Музыка ↗</a>
+      <a href="https://vk.com/vkmusic" target="_blank" rel="noreferrer" title = "VK Music" className="navigation__link">Spotify ↗</a>
+      <a href="https://vk.com/vkmusic" target="_blank" rel="noreferrer" title = "VK Music" className="navigation__link">Apple Music ↗</a>
+      <a href="https://vk.com/vkmusic" target="_blank" rel="noreferrer" title = "VK Music" className="navigation__link">VK Music ↗</a>
     </nav>
   )
 };
