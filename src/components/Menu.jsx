4f1ad71d46add122ddd21228cde32de580e44e81
Alexey Delendik <delendikalexey@gmail.com>
2020-11-04 10:02:47 +0300

    update nav create hover

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -3,10 +3,20 @@ import React from 'react';
 function Menu () {
   return(
     <nav className = "navigation">
-      <a href="https://vk.com/vkmusic" target="_blank" rel="noreferrer" title = "VK Music" className="navigation__link">Яндекс.Музыка ↗</a>
-      <a href="https://vk.com/vkmusic" target="_blank" rel="noreferrer" title = "VK Music" className="navigation__link">Spotify ↗</a>
-      <a href="https://vk.com/vkmusic" target="_blank" rel="noreferrer" title = "VK Music" className="navigation__link">Apple Music ↗</a>
-      <a href="https://vk.com/vkmusic" target="_blank" rel="noreferrer" title = "VK Music" className="navigation__link">VK Music ↗</a>
+      <ul className = "navigation__list">
+        <li className = "navigation__item">
+          <a href="https://vk.com/vkmusic" target="_blank" rel="noreferrer" title = "VK Music" className="navigation__link hover">Яндекс.Музыка ↗</a>
+        </li>
+        <li className = "navigation__item">
+          <a href="https://vk.com/vkmusic" target="_blank" rel="noreferrer" title = "VK Music" className="navigation__link hover">Spotify ↗</a>
+        </li>
+        <li className = "navigation__item">
+          <a href="https://vk.com/vkmusic" target="_blank" rel="noreferrer" title = "VK Music" className="navigation__link hover">Apple Music ↗</a>
+        </li>
+        <li className = "navigation__item">
+          <a href="https://vk.com/vkmusic" target="_blank" rel="noreferrer" title = "VK Music" className="navigation__link hover">VK Music ↗</a>
+        </li>
+      </ul> 
     </nav>
   )
 };
