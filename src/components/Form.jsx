efd211b665d1920a29945033844e6cab10f4ddd2
Kirill Sychevnik <trasco006@gmail.com>
2020-11-06 00:10:56 +0300

    fixed bugs, finished adaptive disign for content block and footer

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -5,13 +5,16 @@ function Form() {
     <form action="" className="form">
       <h2 className="form__title">Форма.</h2>
       <p className="form__subtitle">Заполняя эту форму, вы становитесь частью проекта</p>
-      <input type="text" placeholder="Имя и фамилия автора" name="name" className="form__input"/>
-      <input type="tel" placeholder="Телефон" name="tel" className="form__input"/>
-      <input type="email" placeholder="Почта" name="email" className="form__input"/>
-      <textarea placeholder="Стихи" className="form__input form__input_size_l " name="text_large"/>
+      <input type="text" placeholder="Имя и фамилия автора" name="name" className="form__input" minLength="5" pattern="/^[а-яА-я ._-]+$/" required/>
+      <span id="input-name-error" className="popup__error "></span>
+      <input type="tel" placeholder="Телефон" name="tel" className="form__input" pattern="^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$" required/>
+      <span id="input-tel-error" className="popup__error "></span>
+      <input type="email" placeholder="Почта" name="email" className="form__input" required/>
+      <span id="input-email-error" className="popup__error "></span>
+      <textarea placeholder="Стихи" className="form__input form__input_size_l " name="text_large" required/>
       <div className="form__row">
         <div className="checkbox">
-          <input type="checkbox" id="checkbox_1"/>
+          <input type="checkbox" required id="checkbox_1"/>
           <label htmlFor="checkbox_1">
           </label>
         </div>
@@ -20,8 +23,6 @@ function Form() {
             <a href="#" className="form__link">офертой</a></p>
         </div>
       </div>
-
-
       <button type="submit" className="form__submit-button">Отправить форму</button>
     </form>
   )
