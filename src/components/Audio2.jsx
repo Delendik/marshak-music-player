cf6b5a09aabb9fdacbf0f5a570378bb14bfcdde0
Alexey Delendik <delendikalexey@gmail.com>
2020-11-13 19:26:07 +0300

    player with one track only

--- src/components/Audio2.jsx
+++ src/components/Audio2.jsx
@@ -4,6 +4,9 @@ import PlaylistItem from './PlaylistItem'
 import PlayerTimeline from "./PlayerTimeline"
 import throttle from '../utils/throttling'
 import msToTime from '../utils/convert-ms-min'
+import play1 from '../images/player/play1.svg';
+import pause1 from '../images/player/pause1.svg';
+import openPlayList from '../images/player/openPlayList.svg';
 
 function Audio2 () {
   const [currentTrack, setCurrentTrack] = useState(playList[0]);
@@ -32,63 +35,66 @@ function Audio2 () {
 
   return(
     <div className = "player">
-    <div>
-      <audio 
-        className="player__default"
-        src={currentTrack.file}
-        controls
-        ref={myPlayer}
-        onPlay={onPlay}
-        onPause={onPause}
-        onTimeUpdate = {onTimeUpdate}
-        onLoadedData={() => setDuration(myPlayer.current.duration)}
-      >
-        Your browser doesn't support audio
-      </audio>
-      <button 
-        onClick={() =>{
-          if(isPlaying){
-            myPlayer.current.pause();
-            setIsPlaying(false)
-          } else{
-            myPlayer.current.play();
-            setIsPlaying(true)
-          }
-        }}
-      > {isPlaying ? 'pause' : 'play'} </button>
-    </div>
-    <div>
-      <div className="player__info-part">
-        <div className="player__control">
-          {currentTrack.title} - {currentTrack.author1} feat {currentTrack.author2}
-        </div>
-        <div>{msToTime(Math.round(currentTime))} </div>
-        {/* <div>duration: {msToTime(Math.round(duration))} </div> */}
+      <div className="grid__1">
+        <audio 
+          className="player__default"
+          src={currentTrack.file}
+          controls
+          ref={myPlayer}
+          onPlay={onPlay}
+          onPause={onPause}
+          onTimeUpdate = {onTimeUpdate}
+          onLoadedData={() => setDuration(myPlayer.current.duration)}
+        >
+          Your browser doesn't support audio
+        </audio>
+        <button 
+          className="player__button"
+          onClick={() =>{
+            if(isPlaying){
+              myPlayer.current.pause();
+              setIsPlaying(false)
+            } else{
+              myPlayer.current.play();
+              setIsPlaying(true)
+            }
+          }}
+        > <img src={isPlaying ? pause1 : play1} alt={isPlaying ? 'пауза' : 'старт'} className="player__button-image" /> </button>
       </div>
-    </div>
-    <div className="player__button-playlist"></div>
-    <div></div>
-    <PlayerTimeline 
-      currentTime={currentTime}
-      duration={duration}
-      onClick={time => {
-        myPlayer.current.currentTime=time
-      }}
-    />
-    <div></div>
-      
-      {/* <div className="playlist">
-        <div className="playlist__title">Релизы:</div>
-        {playList.map(item => <PlaylistItem 
-          item={item} 
-          key={item.id}
-          onClick={item => {
-            setCurrentTrack(item)
-            setIsPlaying(false)
-            }} 
+      <div>
+        <div className="player__info-part">
+          <div className="player__control">
+            {currentTrack.title} - {currentTrack.author1} feat {currentTrack.author2}
+          </div>
+          <div className="player__time">{msToTime(Math.round(currentTime))} </div>
+          {/* <div>duration: {msToTime(Math.round(duration))} </div> */}
+        </div>
+      </div>
+      <div className="player__button">
+        <img src={openPlayList} alt="Плейлист" className="player__button-playlist_open" />
+      </div>
+      <div></div>
+      <PlayerTimeline 
+        currentTime={currentTime}
+        duration={duration}
+        onClick={time => {
+          myPlayer.current.currentTime=time
+        }}
+      />
+      <div></div>
+        
+        {/* <div className="playlist">
+          <div className="playlist__title">Релизы:</div>
+          {playList.map(item => <PlaylistItem 
+            item={item} 
+            key={item.id}
+            onClick={item => {
+              setCurrentTrack(item)
+              setIsPlaying(false)
+              }} 
 
-        />)}
-      </div> */}
+          />)}
+        </div> */}
     </div>
   )
 };
